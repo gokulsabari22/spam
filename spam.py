82fbcf479fd3f95665f780b6ddc37fcfc2ba6202
gokulsabari22 <57941940+gokulsabari22@users.noreply.github.com>
2020-03-21 11:43:21 +0530

    Initial commit

---
+++ spam.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+df = pd.read_csv('spam_ham_dataset.csv')
+df.head()
+df = df.drop(['Unnamed: 0', 'label'], axis=1)
+c = df.isnull().sum()
+print(c)
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+import re
+ps = PorterStemmer()
+wordnet = WordNetLemmatizer()
+corpus = []
+for i in range(len(df)):
+    sentence = re.sub('[A-Za-z]', ' ', df['text'][i])
+    sentence = sentence.lower()
+    sentence = sentence.split()
+    sentence = [ps.stem(words) for words in sentence if words not in stopwords.words('english')]
+    sentence = ' '.join(sentence)
+    corpus.append(sentence)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+vector = TfidfVectorizer()
+X = vector.fit_transform(corpus)
+y = df['label_num'
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+from sklearn.naive_bayes import BernoulliNB
+clf = BernoulliNB()
+clf.fit(X_train, y_train)
+predict = clf.predict(X_test)
+print(clf.score(X_train, y_train))
+print(clf.score(X_test, y_test))
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+print(classification_report(predict, y_test))
+print(confusion_matrix(predict, y_test))
+
+
+
+
+
+
